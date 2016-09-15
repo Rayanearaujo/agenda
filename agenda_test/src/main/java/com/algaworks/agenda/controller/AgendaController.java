08fcecb59b4ad0533f8ba2222051fb90803cbcf6
rayanearaujo <rayane.nunes11@gmail.com>
2016-09-15 14:17:54 -0300

    cadastro eventos v1

--- agenda_test/src/main/java/com/algaworks/agenda/controller/AgendaController.java
+++ agenda_test/src/main/java/com/algaworks/agenda/controller/AgendaController.java
@@ -3,17 +3,32 @@ package com.algaworks.agenda.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.agenda.model.Evento;
+import com.algaworks.agenda.model.TipoEvento;
 import com.algaworks.agenda.repository.Eventos;
+import com.algaworks.agenda.service.CadastroEventoService;
 
 @Controller
 @RequestMapping("/agenda")
 public class AgendaController {
-
+	@Autowired
+	private CadastroEventoService cadastroEventoService;
+	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "/agenda/CadastroEvento";
+	public ModelAndView novo(Evento evento) {
+		ModelAndView mv = new ModelAndView("/agenda/CadastroEvento");
+		mv.addObject("tipos", TipoEvento.values());
+		return mv;
+	}
+	
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
+	public ModelAndView cadastarNovoEvento(Evento evento){
+		cadastroEventoService.salvar(evento);
+		return new ModelAndView("redirect:/agenda/novo");
+		//return new ModelAndView();
 	}
 	
 	@Autowired
