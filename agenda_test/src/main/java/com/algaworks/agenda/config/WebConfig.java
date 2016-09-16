f7cda39201e5b8518ad94fc6ea97712530f934ff
laisMayra <lais.mayra92@gmail.com>
2016-09-16 15:42:06 -0300

    login em memoria_+nome no mennu superior

--- agenda_test/src/main/java/com/algaworks/agenda/config/WebConfig.java
+++ agenda_test/src/main/java/com/algaworks/agenda/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -16,6 +17,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404")));
 	}
 	
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry){
+		registry.addRedirectViewController("/", "/agenda");
+		
+	}
 }
 
 
